e1b89fdf2d1bb84298463532c7e569148eaec3ec
DevExpressExampleBot <fakeEmail>
2023-12-25 13:34:12 +0400

    Source auto update [skip ci]

--- VB/CustomizeContextMenus/MainWindow.xaml.vb
+++ VB/CustomizeContextMenus/MainWindow.xaml.vb
@@ -1,14 +1,15 @@
-﻿Imports System.Windows
+Imports System.Windows
 
 Namespace CustomizeContextMenus
+
     ''' <summary>
     ''' Interaction logic for MainWindow.xaml
     ''' </summary>
-    Partial Public Class MainWindow
+    Public Partial Class MainWindow
         Inherits Window
 
         Public Sub New()
-            InitializeComponent()
+            Me.InitializeComponent()
         End Sub
 
         Private Sub bAbout_ItemClick(ByVal sender As Object, ByVal e As DevExpress.Xpf.Bars.ItemClickEventArgs)
@@ -16,7 +17,7 @@ Namespace CustomizeContextMenus
         End Sub
 
         Private Sub Window_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
-            reportDesigner.OpenDocument(New XtraReport1())
+            Me.reportDesigner.OpenDocument(New XtraReport1())
         End Sub
     End Class
 End Namespace
